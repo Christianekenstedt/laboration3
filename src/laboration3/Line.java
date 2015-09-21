10373ba2acac56193b44aa1ba5501dfd0dc0f96e
Christianekenstedt <Christian@n142-p39.kthopen.kth.se>
2015-09-21 10:16:51 +0200

    Line class

---
+++ src/laboration3/Line.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboration3;
+
+/**
+ *
+ * @author Christian
+ */
+public class Line {
+    private double x2;
+    private double y2;
+    
+    public Line(){
+        
+    }
+    
+    public double getX2(){
+        return x2;
+    }
+    
+    public double getY2(){
+        return y2;
+    }
+}
